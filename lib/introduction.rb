66725bd7a4924568020f9355d8bde4927a6b7c83
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-21 17:31:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-#introduction_with_language.rb
+# introduction_with_language.rb
 def introduction_with_language
   puts (Dan, "Ember.js")
 end
